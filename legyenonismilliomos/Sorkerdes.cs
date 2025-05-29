d5052ee4fdb8081e6bd3882cb180bdaa7d2b4ed3
Kristóf Borbára <borbara.kristof@petrik.hu>
2025-05-29 15:49:05 +0200

    sorkerdesek kesz

---
+++ legyenonismilliomos/Sorkerdes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legyenonismilliomos
+{
+    public class Sorkerdes
+    {
+        public string kerdes;
+        public List<string> valaszok;
+        public string helyesValasz;
+        public string kategoria;
+        public Sorkerdes(string kerdes, string valasz1, string valasz2, string valasz3, string valasz4, string helyesValasz, string kategoria)
+        {
+            this.kerdes = kerdes;
+            this.valaszok = new List<string> { valasz1, valasz2, valasz3, valasz4 };
+            this.helyesValasz = helyesValasz;
+        }
+        public override string ToString()
+        {
+            return $"{kerdes}: A:{valaszok[0]} B:{valaszok[1]} C:{valaszok[2]} D:{valaszok[3]}";
+        }
+    }
+}
